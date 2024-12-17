a5b578070be164f526c0ce0a04482376141e64e1
Mikhail-Stepanov410 <mishastepanov1278@gmail.com>
2024-12-17 19:15:49 +0500

    ver_0.0.1

---
+++ scripts.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  slidesPerView: 1,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+
+});
